6cbc2f6284f251fad01b9fc89a9bf7845d2e63d5
zeqk.net <zeqk.net@68e35264-a440-22b2-0b06-18cb4dbc8ce7>
2011-09-15 04:09:36 +0000

    subido projecto

---
+++ trunk/DiccionarioChino/src/diccionariochino/engine/FileEngine.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package diccionariochino.engine;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+/**
+ *
+ * @author zeqk
+ * based in http://www.ehow.com/how_6796642_read-csv-file-java.html
+ */
+public class FileEngine {
+
+    public static TableModel parse(File f) throws FileNotFoundException {
+
+        ArrayList<String> headers = new ArrayList<String>();
+        ArrayList<String> oneDdata = new ArrayList<String>();
+
+        // Get the headers of the table.
+        Scanner lineScan = new Scanner(f);
+        Scanner s = new Scanner(lineScan.nextLine());
+        s.useDelimiter(",");
+
+        while (s.hasNext()) {
+            headers.add(s.next());
+        }
+
+        // Go through each line of the table and add each cell to the ArrayList
+
+        while (lineScan.hasNextLine()) {
+
+            s = new Scanner(lineScan.nextLine());
+            s.useDelimiter(", *");
+
+            while (s.hasNext()) {
+                oneDdata.add(s.next());
+            }
+
+        }
+
+        String[][] data = new String[headers.size()][oneDdata.size()/headers.size()];
+
+        // Move the data into a vanilla array so it can be put in a table.
+
+        for (int x = 0; x < headers.size(); x++) {
+
+            for (int y = 0; y < data[0].length; y++) {
+
+                data[x][y] = oneDdata.remove(0);
+            }
+        }
+
+        // Create a table and return it.
+
+
+        return new DefaultTableModel(data, headers.toArray());
+
+}
+}
